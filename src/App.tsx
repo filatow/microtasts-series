b889e90ffd0bdd5d85bf796b651d18c78ab801b2
evgeniy <not-found@inbox.ru>
2023-12-05 23:13:59 +0300

    Perform preliminary preparation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
 import './App.css';
-import {Header} from "./site/Header";
-import {Body} from "./site/Body";
-import {Footer} from "./site/Footer";
-import {Cars} from "./site/Cars";
 
 function App() {
-    const topCars = [
-        {manufacturer: 'BMW', model: 'm5cs'},
-        {manufacturer: 'Mercedes', model: 'e63s'},
-        {manufacturer: 'Audi', model: 'rs6'}
-    ]
-
     return (
-        <>
-            <Header title={'HEADER'}/>
-            <Body content={'BODY CONTENT'}/>
-            <Cars cars={topCars}/>
-            <Footer title={'FOOTER'}/>
-        </>
+        <div className="App">
+
+        </div>
     )
 }
 
