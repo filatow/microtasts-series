a7cb4d36cc40022e8b3ef569c8141013e54e1804
evgeniy <not-found@inbox.ru>
2023-12-05 23:30:05 +0300

    Do task with useState hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+    // let a = 1
+    const [a, setA] = useState(1);
+    const onClickHandler = () => {
+        setA(a + 1);
+        console.log(a);
+    }
+    const onClickHandler2 = () => {
+        setA(0);
+    }
+
     return (
         <div className="App">
-
+            <h1>{a}</h1>
+            <button onClick={onClickHandler}>number</button>
+            <button onClick={onClickHandler2}>0</button>
         </div>
     )
 }
