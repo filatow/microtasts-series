5cc279213c7cebd91ea1125aef7e8d8ca576277d
evgeniy <not-found@inbox.ru>
2023-12-05 21:29:20 +0300

    Finish 03-Button lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
+import {Button} from "./components/Button";
 
 function App() {
+    const Button2Foo = (subscriber: string, age: number) => {
+        console.log(`I'm ${subscriber}, ${age}`);
+    }
+    const Button1Foo = (subscriber: string) => {
+        console.log(`I'm ${subscriber}`);
+    }
+    const Button3Foo = () => {
+        console.log(`I'm stupid button`);
+    }
+
     return (
         <div className='App'>
-
+            <Button name={'Subscriber'} callBack={() => Button1Foo('Ivan')}/>
+            <Button name={'Subscriber with age'} callBack={() => Button2Foo('Vasia', 21)}/>
+            <Button name={'Stupid Button'} callBack={() => Button3Foo()}/>
         </div>
     )
 }
